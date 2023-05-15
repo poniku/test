efdc07e40d53b26b0fbef912253c518ce357f697
pingu123456 <pingu123456@163.com>
2023-05-15 11:28:37 +0800

    v1.0

---
+++ src/main/java/com/pi/Hello.java
@@ -0,0 +1,11 @@
+package com.pi;
+
+/**
+ * @author pipi
+ * @create 2023-05-15 11:24
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Git Hello");
+    }
+}
