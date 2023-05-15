4be14470bac2530e9273187009dee1d35ae34faf
pingu123456 <pingu123456@163.com>
2023-05-15 11:33:08 +0800

    v1.2

--- src/main/java/com/pi/Hello.java
+++ src/main/java/com/pi/Hello.java
@@ -8,5 +8,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Git Hello");
         System.out.println("Git Hello2");
+        System.out.println("Git Hello2");
+        System.out.println("Git Hello2");
     }
 }
